4f47dda539c4350f73fe905d1114bbcad9fe408e
Retorin <Joakim.friksen.reigstad@gmail.com>
2024-06-07 15:06:19 +0200

    Add project files.

---
+++ Random i C#/Hobbyselector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomHobbyApp
+{
+    public static class HobbySelector
+    {
+        public static string DrawRandomHobby(List<string> hobbies)
+        {
+            var random = new Random();
+            int index = random.Next(hobbies.Count);
+            return hobbies[index];
+        }
+    }
+}
---
+++ Random i C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomHobbyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var hobbies = new List<string> { "Lese bøker", "Spille fotball", "Male", "Lage mat", "Se på filmer" };
+
+           
+            Console.Write("Skriv inn ditt navn: ");
+            var name = Console.ReadLine();
+
+     
+            var randomHobby = HobbySelector.DrawRandomHobby(hobbies);
+
+            
+            Console.WriteLine($"Hei {name}, din tilfeldig valgte hobby er: {randomHobby}");
+        }
+    }
+}
